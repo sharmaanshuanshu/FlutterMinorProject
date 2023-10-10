711306c08f8a148e531675ec5748d8c923d1a382
Ankush <ankush.sharma@sdplabs.com>
2023-10-10 18:22:23 +0530

    Commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+
+    const app = require('./app');
+   const port = 3000;
+   app.get('/',(req,res)=>{
+   res.send('Hello World!');
+   })
+   app.listen(port,()=>{
+   console.log(`Server run on port http://localhost:${port}`);
+   });
